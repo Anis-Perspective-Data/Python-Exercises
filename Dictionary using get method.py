164c4e29093bcabe324abe37717c62b7effc5b52
Anila B <99398151+Anis-Perspective-Data@users.noreply.github.com>
2022-02-20 20:34:18 +0530

    Add files via upload

---
+++ Dictionary using get method.py
@@ -0,0 +1,5 @@
+def dict(a):
+	return 1 if (n==1 or n==0) else n * factorial(n-1);
+	num=5;
+	print('factorial of', num,'is',
+	factorial (num))
---
+++ Minimum.py
@@ -0,0 +1,19 @@
+def minimum(a,b):
+	if a>b:
+		return a
+	else:
+		return b
+
+print(minimum(2,4))
+
+
+
+				
+
+
+
+
+
+
+				
+				
