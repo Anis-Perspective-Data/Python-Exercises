cccd68ae20292c4caa5ce27ccc6cc2d945f5cc0c
Anila B <99398151+Anis-Perspective-Data@users.noreply.github.com>
2022-02-21 17:22:56 +0530

    Add files via upload

---
+++ To generate a random number.py
@@ -0,0 +1,3 @@
+import random
+import random
+print(random.randint(1,9))
---
+++ palindrome or not.py
@@ -0,0 +1,8 @@
+def palindrome(string):
+    if string==string[::-1]:
+        print("it is polindrome")
+    else:
+        ("it is not a polindrome")
+palindrome('amma')
+
+
